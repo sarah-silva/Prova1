83b8befe431749005863dd567b03afa95f455c8a
sarah-silva <52073725+sarah-silva@users.noreply.github.com>
2020-11-13 20:49:48 -0300

    Update validacao.js

--- js/validacao.js
+++ js/validacao.js
@@ -1,4 +1,13 @@
 
+$(document).ready(function() {
+	
+	$("#cpf").mask("000.000.000-00", { placeholder: "___.___.___-__" });
+	$("#cnpj").mask("00.000.000/0000-00", { placeholder: "__.___.___/____-__" });
+	$("#cep").mask("00000-000", { placeholder: "_____-___" });	
+	
+});
+
+
 
 
 function verifica() {
@@ -8,4 +17,19 @@ function verifica() {
 		alert("Por favor, preencha os campos corretamente."); 
 		return; 
 	} 
+}
+
+function mostrarRegistro(){
+	
+	if((document.getElementById("pessoafisica").checked))
+	{
+		document.getElementById("mostraDivCpf").style ="";
+		document.getElementById("mostraDivCnpj").style = "display:none;";
+		
+	}else if ((document.getElementById("pessoajuridica").checked))
+	{
+		document.getElementById("mostraDivCnpj").style ="";
+		document.getElementById("mostraDivCpf").style = "display:none;";
+		
+	}
 }
